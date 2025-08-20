754b0333cbaa5ebb6523e26adc043a56bbfb18dc
harish <harishravi325@gmail.com>
2025-08-20 21:13:55 +0530

    index html only change

--- todo.js
+++ todo.js
@@ -1,2 +1,5 @@
-let taskBox=document.querySelector("#addtask");
-console.log(taskBox);
+// ---condition checking-----
+var numericExpression=/^[0-9]+$/; if(elem.value.match(numericExpression));
+var alphaExp=/^[a-zA-Z]+$/; if(elem.value.match(alphaExp));
+var alphaExp=/^[0-9, a-z a-z, 0-9, A-Z A-Z, - 0-9 . ]+$/; if(elem.value.match(alphaExp));
+var emailExp=/^[0-9 a-z . a-z 0-9]+\@[a-z]+\.[a-z]{2,4}$/; if(elem.value.match(emailExp));
